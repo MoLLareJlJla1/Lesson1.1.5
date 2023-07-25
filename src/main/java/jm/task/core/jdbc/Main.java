a4ddd4bd860ba585359738ba34eba680973e01b1
MoLLareJlJla1 <MoLLareJlJla1@mail.ru>
2023-07-26 00:56:36 +0300

    Done

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,20 +1,16 @@
 package jm.task.core.jdbc;
 
-import jm.task.core.jdbc.model.User;
 import jm.task.core.jdbc.service.UserService;
 import jm.task.core.jdbc.service.UserServiceImpl;
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        UserService userService = new UserServiceImpl();
-        userService.createUsersTable();
-        userService.cleanUsersTable();
-        userService.saveUser("sergei", "safronov", (byte) 30);
-        userService.saveUser("igor", "igorev", (byte) 41);
-        userService.saveUser("gfdgdf", "gfdgdf", (byte) 45);
-        userService.removeUserById(1);
-        List<User> users = userService.getAllUsers();
-        System.out.println(users);
+        UserService us = new UserServiceImpl();
+        us.createUsersTable();
+        us.saveUser("Sergei", "Safronov", (byte) 39);
+        us.saveUser("Sergei", "Safronov", (byte) 39);
+        us.saveUser("Sergei", "Safronov", (byte) 39);
+        us.removeUserById(2);
+
     }
 }
--- src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
+++ src/main/java/jm/task/core/jdbc/dao/UserDaoHibernateImpl.java
@@ -1,44 +1,72 @@
 package jm.task.core.jdbc.dao;
 
 import jm.task.core.jdbc.model.User;
+import jm.task.core.jdbc.util.Util;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDaoHibernateImpl implements UserDao {
-
-
-    public UserDaoHibernateImpl() {
-    }
-
+    private static SessionFactory factory = Util.getSessionFactory();
 
     @Override
     public void createUsersTable() {
-
+        try (Session session = factory.getCurrentSession();) {
+            session.beginTransaction();
+            session.createSQLQuery("CREATE TABLE IF NOT EXISTS User (id INT PRIMARY KEY AUTO_INCREMENT," +
+                    " name VARCHAR(45), lastname VARCHAR(45), age INT)").executeUpdate();
+            session.getTransaction().commit();
+        }
     }
 
     @Override
     public void dropUsersTable() {
-
+        try (Session session = factory.getCurrentSession();) {
+            session.beginTransaction();
+            session.createSQLQuery("DROP TABLE IF EXISTS User").executeUpdate();
+            session.getTransaction().commit();
+        }
     }
 
     @Override
     public void saveUser(String name, String lastName, byte age) {
-
+        try (Session session = factory.getCurrentSession();) {
+            session.beginTransaction();
+            User user = new User(name, lastName, age);
+            session.saveOrUpdate(user);
+            session.getTransaction().commit();
+        }
     }
 
     @Override
     public void removeUserById(long id) {
-
-
+        try (Session session = factory.getCurrentSession();) {
+            session.beginTransaction();
+            User user = session.get(User.class, id);
+            session.delete(user);
+            session.getTransaction().commit();
+        }
     }
 
     @Override
     public List<User> getAllUsers() {
-return null;
+        List<User> user = new ArrayList<>();
+        try (Session session = factory.getCurrentSession()) {
+            session.beginTransaction();
+            user = session.createQuery("from User").getResultList();
+            session.getTransaction().commit();
+            return user;
+        }
     }
 
     @Override
     public void cleanUsersTable() {
-
+        try (Session session = factory.getCurrentSession();) {
+            session.beginTransaction();
+            session.createQuery("delete User").executeUpdate();
+            session.getTransaction().commit();
+        }
     }
 }
